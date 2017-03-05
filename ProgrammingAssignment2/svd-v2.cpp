0febe68439bd6000bc1c329aed57712294a1b56f
Kyle Heaton <kyle.heaton@myresman.com>
2017-03-04 23:23:14 -0700

    Updated thread count to 32

--- ProgrammingAssignment2/svd-v2.cpp
+++ ProgrammingAssignment2/svd-v2.cpp
@@ -70,7 +70,7 @@ int array_size(T* array)
 
 int main(int argc, char *argv[])
 {
-	int numberOfThreads = 16;
+	int numberOfThreads = 32;
 
 	omp_set_dynamic(0);
 	omp_set_num_threads(numberOfThreads);
--- ProgrammingAssignment2/svd.cpp
+++ ProgrammingAssignment2/svd.cpp
@@ -40,7 +40,7 @@ template <typename T> double sgn(T val)
 
 int main (int argc, char* argv[]) {
 
-	int numberOfThreads = 16;
+	int numberOfThreads = 32;
 
 	omp_set_dynamic(0);
 	omp_set_num_threads(numberOfThreads);
